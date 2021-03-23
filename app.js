d1a3bc223833048c533cf4bcc2aa1ca497bca3b9
noavrd <noavrd@gmail.com>
2021-03-23 19:59:13 +0200

    add get

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
+const tickets = require("./seeds/tickets/tickets.json")
 
+
+app.use(cors());
+app.use(express.json());
 app.use(express.static("client/build"));
+
+app.get("/", (request, response) => {
+    response.send("API server")
+})
+
+app.get("/api/tickets",  (request, response) => {
+    response.send(tickets)
+ })
 
 module.exports = app;
